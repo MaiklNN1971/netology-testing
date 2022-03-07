2d288d900cad37e42f10b4685a0543aed8337dbb
Михаил Воротников <wmw@mail.ru>
2022-03-07 11:17:33 +0300

    версия от 07.03.2022

--- oop_1.py
+++ oop_1.py
@@ -5,9 +5,9 @@
 # some_person.__dict__['age'] = 30
 # print (some_person.age + len(some_person.__dict__))
 
-class Income:
-    def __init__(self,id_):
-        self.id_ = id_
-        id_ = 100
-income_1 = Income(1000)
-print(income_1.id_)
+# class Income:
+#     def __init__(self,id_):
+#         self.id_ = id_
+#         id_ = 100
+# income_1 = Income(1000)
+# print(income_1.id_)
